5d8ab27d0a585767c315610a496e7a8958597f96
M. S. Kohli <email.mskohli@gmail.com>
2019-08-05 23:30:51 +1000

    Config changes

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,21 +1,19 @@
 /// <reference types="Cypress" />
 
 describe('Window Variable', () => {
-    it('Fetch Window Variable', () => {
-        cy.visit('./test.html');
-        cy.window()
-            .its('global')
-            .should('equal', 1000);
-    });
+  it('Fetch Window Variable', () => {
+    cy.visit('./test.html');
+    cy.window()
+      .its('global')
+      .should('equal', 1000);
+  });
 });
 
 describe('API Testing', () => {
-    it.only('Send Request', () => {
-        cy.request(
-            'http://dummy.restapiexample.com/api/v1/employee/15658'
-        ).then(response => {
-            expect(response.status).eq(200);
-            expect(response.body).contains('id');
-        });
+  it.only('Send Request', () => {
+    cy.request('http://dummy.restapiexample.com/api/v1/employee/15658').then(response => {
+      expect(response.status).eq(200);
+      expect(response.body).contains('id');
     });
+  });
 });
